2ec22837ca9197a8aa8b12656fc48bb45f4d5676
Solerny <sf222844@gmail.com>
2024-05-23 11:14:49 -0400

    Avance
    Se agregaron cambios a la interfaz, se crearon imagenes para que se vean bueno assert vectors

---
+++ app/src/main/java/com/example/recetas/Adapters/RandomRecipeAdapter.java
@@ -0,0 +1,16 @@
+package com.example.recetas.Adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RandomRecipeAdapter {
+
+}
+class RandomRecipeViewHolder extends RecyclerView.ViewHolder{
+
+    public RandomRecipeViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
